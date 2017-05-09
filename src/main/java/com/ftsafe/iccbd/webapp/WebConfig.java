8f72f2d7ff07a6507db57c3942cf7a70c0af8383
qingyuan <qingyuan.ke@foxmail.com>
2017-05-09 15:10:57 +0800

    first commit

---
+++ src/main/java/com/ftsafe/iccbd/webapp/WebConfig.java
@@ -0,0 +1,9 @@
+package com.ftsafe.iccbd.webapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+@Configuration
+public class WebConfig extends AnnotationConfigWebApplicationContext {
+
+}
---
+++ src/main/java/com/ftsafe/iccbd/webapp/service/UserService.java
@@ -0,0 +1,5 @@
+package com.ftsafe.iccbd.webapp.service;
+
+public interface UserService {
+
+}
