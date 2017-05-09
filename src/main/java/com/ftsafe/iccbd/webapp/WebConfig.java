3ba01bd2c1e96c36313e1304ce15f58606ceff38
qingyuan <qingyuan.ke@foxmail.com>
2017-05-09 19:49:26 +0800

    添加spring-context依赖

--- src/main/java/com/ftsafe/iccbd/webapp/WebConfig.java
+++ src/main/java/com/ftsafe/iccbd/webapp/WebConfig.java
@@ -1,9 +1,12 @@
 package com.ftsafe.iccbd.webapp;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 @Configuration
+@ComponentScan
 public class WebConfig extends AnnotationConfigWebApplicationContext {
 
 }
